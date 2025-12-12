a6e36e3faf45547e027ca0b234e84b0530541f6a
Nicolas Fleitas <nicofleitaspy@gmail.com>
2025-12-12 10:15:59 -0300

    Crea un modelo Link con los métodos getAll, create, delete

---
+++ models/Link.js
@@ -0,0 +1,30 @@
+const db = require('../config/database');
+
+const Link = {
+    // Obtiene todos los enlaces y ordena en memoria (es más eficiente)
+    getAll: (callback) => {
+        const sql = 'SELECT * FROM links ORDER BY votes DESC';
+        db.all(sql, [], (err, rows) => {
+            // Si hay error, 'err' tendrá datos, si no, 'rows' tendrá la lista
+            callback(err, rows);
+        });
+    },
+
+    // Crear un nuevo enlace asociado a un tema específico (topic_id)
+    create: (topic_id, title, url, callback) => {
+        const sql = 'INSERT INTO links (topic_id, title, url) VALUES (?, ?, ?)';
+        db.run(sql, [topic_id, title, url], function (err) {
+            callback(err, this ? this.lastID : null);
+        });
+    },
+
+    // Eliminar un enlace por su ID
+    delete: (id, callback) => {
+        const sql = 'DELETE FROM links WHERE id = ?';
+        db.run(sql, [id], (err) => {
+            callback(err);
+        });
+    }
+};
+
+module.exports = Link;
