f754b4e65b51bee34f47e4a3fd54387583fe81ac
Nicolas Fleitas <nicofleitaspy@gmail.com>
2025-12-12 10:22:08 -0300

    feat: Agregar rutas para crear y eliminar links

--- routes/topicRoutes.js
+++ routes/topicRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const topicController = require('../controllers/topicController');
 
-// Definición de rutas
+// RUTAS DE TEMAS
 router.get('/', topicController.index);
 router.post('/temas', topicController.store);
 router.post('/temas/delete/:id', topicController.destroy);
+
+// RUTAS PARA ENLACES
+// :id representa el ID del tema donde guardaremos el enlace
+router.post('/temas/:id/enlaces', topicController.storeLink);
+// :id es el ID del ENLACE que vamos a borrar
+router.post('/enlaces/delete/:id', topicController.destroyLink);
 
 module.exports = router
